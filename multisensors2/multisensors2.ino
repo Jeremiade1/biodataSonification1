e60fa4ce0b953124661893f91b6af84f7646cb36
Jeremiade1 <jeremie.brodeur@hotmail.com>
2023-06-20 16:12:41 -0400

    BiodataV3

--- multisensors2/multisensors2.ino
+++ multisensors2/multisensors2.ino
@@ -9,8 +9,14 @@ void loop() {
   //Read analaog inputs one by one and send them to Pure Data
   for(int i = 0; i < 2 ; i++){
     val[i]= analogRead(i);
-Serial.print(val[i]); 
-Serial.print(" ");
+    if(i==0) {
+      int lumMap = map(1023-val[i], 400,1023, 0, 1023);
+      Serial.print(lumMap); 
+    }
+    else{
+    Serial.print(val[i]); 
+    }
+    Serial.print(" ");
 }   
 Serial.println();
 delay(50); 
